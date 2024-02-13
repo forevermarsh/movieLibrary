7214195613ca24bb7a7c580eeb49e83fa47e9a4f
forevermarsh <marshmansur004@gmail.com>
2024-02-13 19:49:29 +0300

    trial thing

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,9 @@ function App() {
 
   return (
     <div style={{width: '100%'}}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Movies />} />
-          <Route path="/movies" element={<Movies />}/>
-          <Route path="/series" element={<Series />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/preview" element={<Preview />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-        </BrowserRouter>
+<div style={{width: '100%', height: '50vh', backgroundColor: 'purple'}}>
+
+</div>
     </div>
   );
 }
