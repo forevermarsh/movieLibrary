73b123f3b0f139b2a0ab6c66b6d14f87c0154c10
forevermarsh <marshmansur004@gmail.com>
2024-02-13 20:39:42 +0300

    again

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Movies from "./pages/movies";
 import Series from "./pages/series";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  
   return (
     <div style={{width: '100%', height: '100vh'}}>
       <BrowserRouter>
