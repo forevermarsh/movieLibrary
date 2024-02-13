75b645b122678547152a597d842f8c3c8238bf70
forevermarsh <marshmansur004@gmail.com>
2024-02-13 20:39:22 +0300

    do me right

---
+++ src/pages/series.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Series() {
+    return(
+        <>
+        <div>
+            <div style={{width: '100%', height: '100vh', backgroundColor: 'pink'}}></div>
+        </div>
+        </>
+    )
+}
+export default Series;
